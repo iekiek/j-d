ebcac75b08683b1d26e946af72085429ce151bf8
iekiek <76714295+iekiek@users.noreply.github.com>
2021-01-22 20:17:52 +0800

    Add files via upload

---
+++ jd-base-3/README.md
@@ -0,0 +1,41 @@
+## 请仔细阅读 [WIKI](https://github.com/EvineDeng/jd-base/wiki) 和各文件注释，95%的问题都能找到答案
+
+## 如有帮助到你，请点亮Star
+
+## 如有二次使用，请注明来源
+
+本脚本是 [https://github.com/LXK9301/jd_scripts](https://github.com/LXK9301/jd_scripts) 和 [https://github.com/shylocks/Loon](https://github.com/shylocks/Loon) 的shell套壳工具。
+
+[LXK9301/jd_scripts](https://github.com/LXK9301/jd_scripts)：主要是长期任务。
+
+[shylocks/Loon](https://github.com/shylocks/Loon)：主要是短期任务、一次性任务，正因为是短期的和一次性的，所以经常会有报错，报错就报错了，不要催我也不要去催[shylocks](https://github.com/shylocks)大佬。
+
+## 适用于以下系统
+
+- ArmBian/Debian/Ubuntu/OpenMediaVault/CentOS/Fedora/RHEL等Linux系统
+
+- OpenWRT
+
+- Android
+
+- MacOS
+
+- Docker
+
+## 说明
+
+1. 宠汪汪赛跑助力先让用户提供的各个账号之间相互助力，助力完成你提供的所有账号以后，再给我和lxk0301大佬助力，每个账号助力后可得30g狗粮。
+
+2. 将部分临时活动修改为了我的邀请码，已取得lxk0301大佬的同意。
+
+## 更新日志
+
+> 只记录大的更新，小修小改不记录。
+
+2020-01-21，增加shylocks/Loon脚本。
+
+2021-01-15，如果本机上安装了pm2，则挂机程序以pm2启动，否则以nohup启动。
+
+## Star趋势
+
+[![Stargazers over time](https://starchart.cc/EvineDeng/jd-base.svg)](https://starchart.cc/EvineDeng/jd-base)
---
+++ jd-base-3/export_sharecodes.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+## Author: Evine Deng
+## Source: https://github.com/EvineDeng/jd-base
+## Modified： 2021-01-22
+## Version： v3.5.1
+
+## 路径、环境判断
+ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
+LogDir=${ShellDir}/log
+[[ ${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT} ]] && Opt="P" || Opt="E"
+
+## 导出互助码的通用程序
+function Cat_Scodes {
+  if [ -d ${LogDir}/jd_$1 ] && [[ $(ls ${LogDir}/jd_$1) != "" ]]; then
+    cd ${LogDir}/jd_$1
+    for log in $(ls -r); do
+      case $1 in
+        bookshop)
+          codes=$(perl -pe "s|信息获取成功\n||g" ${log} | grep -${Opt} "您的好友助力码为" | perl -pe "{s|您的好友助力码为||g; s|用户||g}")
+          ;;
+        cash)
+          codes=$(perl -0777 -pe "s|\*+\n+||g" ${log} | grep -${Opt} "您的助力码为" | perl -pe "{s|\*+开始||g; s|您的助力码为|：|g}")
+          ;;
+        *)
+          codes=$(grep -${Opt} $2 ${log} | perl -pe "s| ||")
+          ;;
+      esac
+      [[ ${codes} ]] && break
+    done
+    [[ ${codes} ]] && echo ${codes} || echo "从日志中未找到任何互助码..."
+  else
+    echo "还没有产生日志..."
+  fi
+}
+
+## 汇总
+function Cat_All {
+  echo -e "本脚本从最后一个正常的日志中寻找互助码，某些账号缺失则代表在最后一个正常的日志中没有找到。\n\n本脚本只搜索长期活动的互助码，短期活动的互助码请直接在原日志中查看。"
+  for ((i=1; i<${#Name1[*]}; i++)); do
+    echo -e "\n${Name2[i]}："
+    Cat_Scodes "${Name1[i]}" "的${Name2[i]}好友互助码" | perl -pe "s|的${Name2[i]}好友互助码||g"
+  done
+}
+
+## 执行并写入日志
+LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
+LogFile="${LogDir}/export_sharecodes/${LogTime}.log"
+Name1=(fruit pet plantBean dreamFactory jdfactory crazy_joy jdzz jxnc bookshop cash)
+Name2=(东东农场 东东萌宠 京东种豆得豆 京喜工厂 东东工厂 crazyJoy任务 京东赚赚 京喜农场 口袋书店 签到领现金)
+[ ! -d "${LogDir}/export_sharecodes" ] && mkdir -p ${LogDir}/export_sharecodes
+Cat_All | perl -pe "{s|京东种豆|种豆|; s|crazyJoy任务|疯狂的JOY|; s| |\n|g}" | tee ${LogFile}
